09956e5ca74520aba7f82467c318bb22b1fb73ce
AntEugene <Tosvering@gmail.com>
2020-11-06 17:19:33 +0000

    New features

--- RaisaRun/views.py
+++ RaisaRun/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import NameForm
  
 def index(request):
     data = {}
     return render(request, "index.html", context=data)
+
+def signup(request):
+	data = {}
+	return render(request, "index.html", context=data)
+
+def images(request):
+	if request.method == "POST":
+		form = NameForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/thanks/')
+	else:
+		form = NameForm()
+
+	return render(request, "index.html", {'form': form})
