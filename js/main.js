74244828767496fa865d1e8cae787f616d1a6ecc
Maria <mariaigorevna1206@gmail.com>
2020-11-12 12:09:16 +0700

    Chenges 12.11.2020

--- js/main.js
+++ js/main.js
@@ -12,32 +12,46 @@ $(function () {
     $('.menu ul').slideToggle();
   });
   $(document).ready(function() {
-    $(".fancybox").fancybox({
-      openEffect	: 'none',
-      closeEffect	: 'none'
+    $("#single_1").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+
+    $("#single_2").fancybox({
+    	openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
     });
-  });
+
+    $("#single_3").fancybox({
+    	openEffect : 'none',
+    	closeEffect	: 'none',
+    	helpers : {
+    		title : {
+    			type : 'outside'
+    		}
+    	}
+    });
+
+    $("#single_4").fancybox({
+    	helpers : {
+    		title : {
+    			type : 'over'
+    		}
+    	}
+    });
+});
 
   $('#asd').html('<img src="img/photos/photo_2.jpg"../>');
 
-
-    $('.minimized').click(function(event) {
-      var i_path = $(this).attr('src');
-      $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
-      $('#magnify').css({
-       left: ($(document).width() - $('#magnify').outerWidth())/2,
-       // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
-              top: ($(window).height() - $('#magnify').outerHeight())/2
-     });
-      $('#overlay, #magnify').fadeIn('fast');
-    });
-    
-    $('body').on('click', '#close-popup, #overlay', function(event) {
-      event.preventDefault();
-      $('#overlay, #magnify').fadeOut('fast', function() {
-        $('#close-popup, #magnify, #overlay').remove();
-      });
-    });
- 
+  
 
 });
